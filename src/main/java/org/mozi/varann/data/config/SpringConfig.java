5b2179df3a5696715a8df9e841669a826161e423
Abdulrahman Semrie <hsamireh@gmail.com>
2019-11-26 12:03:35 +0300

    Add ignite config

--- src/main/java/org/mozi/varann/data/config/SpringConfig.java
+++ src/main/java/org/mozi/varann/data/config/SpringConfig.java
@@ -3,9 +3,15 @@ package org.mozi.varann.data.config;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.springdata20.repository.config.EnableIgniteRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 /**
  * The bean configuration to get Ignite instance
@@ -17,7 +23,14 @@ public class SpringConfig {
 
     @Bean
     public Ignite igniteInstance(){
-        return Ignition.start("example-cache.xml");
+        TcpDiscoverySpi spi = new TcpDiscoverySpi();
+        TcpDiscoveryVmIpFinder tcpVmFinder = new TcpDiscoveryVmIpFinder();
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        tcpVmFinder.setAddresses(Arrays.asList("88.198.22.185"));
+        spi.setIpFinder(tcpVmFinder);
+        spi.setJoinTimeout(3000);
+        cfg.setDiscoverySpi(spi);
+        return Ignition.start(cfg);
     }
 
 }
